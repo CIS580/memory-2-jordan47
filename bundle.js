61ee18a7cfdf9cf62e024bdb34e86bc06f36a01e
jordan47 <jordan47@win.cs.ksu.edu>
2016-09-28 15:20:35 -0500

    Stuff from class 9/28 - changes in bundle.js

--- bundle.js
+++ bundle.js
@@ -15,15 +15,63 @@ var cards = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
 var board = [];
 while(cards.length > 0) {
   var index = Math.floor(Math.random() * (cards.length - 1));
-  board.push({card: cards[index], flip: true});
+  board.push({card: cards[index], flip: false});
   cards.splice(index, 1);
 }
+var state = "waiting for click 1";
+var scores = [0,0];
+var player = 0;
+var card1;
+
 console.log(board);
 
 // TODO: Place the cards on the board in random order
 
 canvas.onclick = function(event) {
   event.preventDefault();
+	var x = Math.floor((event.clientX - 3)/165);
+	var y = Math.floor((event.clientY - 3)/165);
+	
+	var card = board[y * 6 + x];
+	if(!card || card.flip) return;
+	card.flip = true;
+	switch (state)
+	{
+		case "waiting for click 1":
+			card1 = card;
+			state = "waiting for click 2";
+			break;
+		case "waiting for click 2":
+			if(card1.card == card.card)
+			{
+				scores[player]++;
+				state = "waiting for click 2";
+			}
+			else
+			{
+				card1.flip = false;
+				card.flip = false;
+				player = +!player;
+			}
+			card2 = card;
+			state = "waiting for click 1";
+			setTimeout(funtion()
+			{
+				if(card1.card == card2.card)
+				{
+					scores[player]++;
+				}
+				else
+				{
+					card1.flip = false;
+					card2.flip = false;
+					player = +!player;
+				}
+				
+			}
+			break;
+	}
+	
   // TODO: determine which card was clicked on
   // TODO: determine what to do
 }
